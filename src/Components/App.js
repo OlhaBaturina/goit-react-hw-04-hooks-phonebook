226aa5879570ffdc0eaf3cad772d7885a49937df
Olha Baturina <75169470+OlhaBaturina@users.noreply.github.com>
2021-08-11 02:28:17 +0300

    Выносит хук локал сторидж в отдельный файл

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,39 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Form } from './Form/Form';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Contacts from './Contact/Contact';
 import Filter from './Filter/Filter';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 import s from './App.css';
 
-const useLocalStorage = (key, defaultValue) => {
-    const [state, setState] = useState(() => {
-        return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-    });
-
-    useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(state));
-    }, [key, state]);
-
-    return [state, setState];
-};
-
 export function App() {
-    const [contacts, setContacts] = useLocalStorage('contacts', {});
+    const [contacts, setContacts] = useLocalStorage('contacts', [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]);
+
     const [filterName, setFilterName] = useState('');
 
     const getSubmitData = data => {
-        if (
-            contacts.find(
-                contact =>
-                    contact.name.toLowerCase() === data.name.toLowerCase(),
-            )
-        ) {
-            toast.error('This name is also here!');
-            return;
-        }
-
-        setContacts(prevState => [...prevState, data]);
+        (contacts.find(
+            contact => contact.name.toLowerCase() === data.name.toLowerCase(),
+        ) &&
+            toast.warning('This name already exists!')) ??
+            setContacts(prevState => [...prevState, data]);
     };
 
     const handelDelete = data =>
